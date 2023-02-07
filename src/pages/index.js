b944b1f5139ba68b5387b34b181cbe7e9ecdf85d
Dean <deanscottg@gmail.com>
2023-02-07 13:41:10 -0500

    created 404 page and styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,19 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div>
-      <h1>Sam Santamaria Art</h1>
-        <h2>Fine Art out of Tampa Florida</h2>
-          <p>Originally from the Phillippines and having moved to the states in bla bla bla</p>
+      <h1 className={ styles.title }>Sam Santamaria Art</h1>
+        <h2 className={ styles.text }>Fine Art out of Tampa Florida</h2>
+          <p className={ styles.text }>Originally from the Phillippines and having moved to the states in bla bla bla</p>
+          <Link  className={ styles.btn} href="/gallery">Check out my latest work!</Link>
     </div>
   )
 }
