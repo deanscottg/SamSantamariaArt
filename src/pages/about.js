3d54add7e6345ac6e01c62d02cee81d0b0386d54
Dean <deanscottg@gmail.com>
2023-02-14 19:35:09 -0500

    removing css files and ref's

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>  
+            <h1>About</h1>
+            <p>Sam Santamaria: About the artist</p>
+            <p>Originally from the Phillippines and having moved to the states in bla bla bla</p>
+        </div>
+     );
+}
+ 
+export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
 import Link from 'next/link'
@@ -11,10 +10,10 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div>
-      <h1 className={ styles.title }>Sam Santamaria Art</h1>
-        <h2 className={ styles.text }>Fine Art out of Tampa Florida</h2>
-          <p className={ styles.text }>Artist | Photographer | Author </p>
-          <Link  className={ styles.btn } href="/gallery">Check out my latest work!</Link>
+      <h1 className="title" >Sam Santamaria Art</h1>
+        <h2 className="text">Fine Art out of Tampa Florida</h2>
+          <p className="text" >Artist | Photographer | Author </p>
+          <Link  className="btn" href="/gallery">Check out my latest work!</Link>
     </div>
   )
 }
