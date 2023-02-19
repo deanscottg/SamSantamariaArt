4beade654b2cb1fa6d2bddeda50351356290b816
Dean <deanscottg@gmail.com>
2023-02-18 20:12:13 -0500

    custom and base tailwind styles

--- components/ArtCard.tsx
+++ components/ArtCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 const ArtCard = () => {
     return (
         <div className="container">
-                <h1>Painting name</h1>
+                <h2>Painting name</h2>
                 <Image alt="squidward" src="/squidward.jpg" width={300} height={500}/>
                 <p>
                     This is all the info about this painting
