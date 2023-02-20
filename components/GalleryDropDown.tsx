27add907f10be82398d93c1144ca1346fcb0875f
Dean <deanscottg@gmail.com>
2023-02-20 18:26:07 -0500

    added icon to dropdown

--- components/GalleryDropDown.tsx
+++ components/GalleryDropDown.tsx
@@ -5,8 +5,12 @@ const GalleryDropDown = () => {
     return ( 
         <button 
         onClick={() => setIsOpen((prev) => !prev)}
-        className="nav-link-btn">
+        className="nav-link-btn flex flex-row">
         Gallery
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+
             {isOpen && (
             <div className="bg-gray-800 absolute top-20 flex flex-col items start rounded-lg p-2">
                 <Link className="gallery-dropdown-item" href="/gallery/painting"> Paintings</Link>
