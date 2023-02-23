ec84ad15a7812a3777c584e4d7d51a15887409e2
Dean <deanscottg@gmail.com>
2023-02-23 16:46:20 -0500

    styled featured banner, fixed contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,6 @@
 const Contact = () => {
     return (
-        <div className="page-container">
+        <div className="page-container h-screen">
             <h1>Contact</h1>
             <p>You can reach me here</p>
         </div>
