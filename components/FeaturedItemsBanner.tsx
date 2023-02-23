b073c588c6194a843c8a763766b0e71368c5edb8
Dean <deanscottg@gmail.com>
2023-02-23 16:35:31 -0500

    Re styled index, started featured banner

---
+++ components/FeaturedItemsBanner.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+const FeaturedItemsBanner = () => {
+    return ( 
+    <div className="m-0 w-screen bg-white h-200 flex flex-col rounded-sm">
+        <div>
+            <h1>Featured Works</h1>
+        </div>
+        <div className="mx-32 grid grid-cols-1 content-center">
+            <Image
+            src="/featured1.jpg"
+            alt="featured1"
+            height={300}
+            width={300}
+            className="p-6 justify-center"
+            >
+        </Image>
+        </div>
+        
+        {/* <Image 
+        src='/featured1.jpg' 
+        alt="featured1" 
+        height={650} 
+        width={650}></Image> */}
+    </div> );
+}
+ 
+export default FeaturedItemsBanner;
