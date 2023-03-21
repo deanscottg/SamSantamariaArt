2c24f6f3958b5d9099e53c20925d11e9bd63c457
Kenton Duprey <kentonduprey@icloud.com>
2023-03-20 20:11:03 -0400

    Updating schemas on seriesid index page

--- app/src/pages/gallery/[seriesid]/index.tsx
+++ app/src/pages/gallery/[seriesid]/index.tsx
@@ -1,58 +1,43 @@
 import { GetStaticPaths, GetStaticProps } from "next";
-import ArtCard from "../../../../components/ArtCard";
 import { groq } from "next-sanity";
+import { z } from "zod";
+import ArtCard from "../../../../components/ArtCard";
 import { nextSanityClient } from "../../../../lib/client";
 import { Series } from "../../../../types/types";
 import {
-  seriesSchema,
-  dimensionSchema,
-  imageSchema,
+	dimensionSchema,
+	imageSchema,
+	paintingSchema,
+	seriesSchema,
 } from "../../../../types/zodSchemas";
-import { z } from "zod";
-
-const paintingSchema = z.object({
-  _id: z.string().optional(),
-  name: z.string(),
-  dimensions: z.array(dimensionSchema),
-  image: imageSchema,
-});
-const ommittedSeriesSchema = z.object({
-  _createdAt: z.string().optional(),
-  _id: z.string(),
-  _rev: z.string().optional(),
-  _type: z.string().optional(),
-  _updatedAt: z.string().optional(),
-  name: z.string(),
-  paintings: z.array(paintingSchema),
-});
-
-const mergedSeriesSchema = ommittedSeriesSchema.merge(paintingSchema);
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const res = await nextSanityClient.fetch(groq`*[_type == 'series'] { _id }`);
-  const pathsData = z
-    .array(
-      z.object({
-        _id: z.string(),
-      })
-    )
-    .parse(res);
+	const res = await nextSanityClient.fetch(
+		groq`*[_type == 'series'] { _id }`
+	);
+	const pathsData = z
+		.array(
+			z.object({
+				_id: z.string(),
+			})
+		)
+		.parse(res);
 
-  const paths = pathsData.map((series) => {
-    return { params: { seriesid: series._id } };
-  });
+	const paths = pathsData.map((series) => {
+		return { params: { seriesid: series._id } };
+	});
 
-  return {
-    paths,
-    fallback: false,
-  };
+	return {
+		paths,
+		fallback: false,
+	};
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  if (!params?.seriesid) return { notFound: true };
+	if (!params?.seriesid) return { notFound: true };
 
-  const seriesDataResponse = await nextSanityClient.fetch(
-    groq`*[_id == '${params.seriesid}'][0]{
+	const seriesDataResponse = await nextSanityClient.fetch(
+		groq`*[_id == '${params.seriesid}'][0]{
     name,
     paintings[]->{
       name,
@@ -69,48 +54,37 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       }
     }
 }`
-  );
-  console.log(params.seriesid, "test");
-  console.log(seriesDataResponse, "check");
+	);
+	console.log(params.seriesid, "seriesId");
+	console.log(seriesDataResponse, "Data");
 
-  const seriesData = mergedSeriesSchema
-    // Removing these fields from the schema
-    // Not testing data response for these fields
-    .omit({
-      _createdAt: true,
-      _rev: true,
-      _type: true,
-      _id: true,
-      _updatedAt: true,
-    })
-    // Verifying the data from the api
-    .parse(seriesDataResponse);
+	const seriesData = seriesSchema.parse(seriesDataResponse);
 
-  return {
-    // Returing clean data
-    props: { seriesData },
-  };
+	return {
+		// Returing clean data
+		props: { seriesData },
+	};
 };
 
 const Seriesid = ({
-  seriesData,
+	seriesData,
 }: {
-  seriesData: Omit<
-    Series,
-    "_createdAt" | "_rev" | "_type" | "_id" | "_updatedAt"
-  >;
+	seriesData: Omit<
+		Series,
+		"_createdAt" | "_rev" | "_type" | "_id" | "_updatedAt"
+	>;
 }) => {
-  return (
-    <div className="page-container">
-      <h1>{seriesData.name}</h1>
-      <p>Brief description of {seriesData.name}</p>
-      <div className="art-card-grid">
-        {seriesData.paintings.map((painting, i) => (
-          <ArtCard paintingData={painting} key={i} />
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="page-container">
+			<h1>{seriesData.name}</h1>
+			<p>Brief description of {seriesData.name}</p>
+			<div className="art-card-grid">
+				{seriesData.paintings.map((painting, i) => (
+					<ArtCard paintingData={painting} key={i} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Seriesid;
