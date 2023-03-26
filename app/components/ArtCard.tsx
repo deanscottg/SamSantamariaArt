1067922cdce598eef9865aeb86f6011784b7cf99
Dean <deanscottg@gmail.com>
2023-03-26 12:28:04 -0400

    cleaned up nav bar

--- app/components/ArtCard.tsx
+++ app/components/ArtCard.tsx
@@ -1,36 +1,38 @@
 import NextImage from "next/image";
 import Link from "next/link";
-import z from "zod";
 import { Painting, Series, WithRequired } from "../types/types";
 import { paintingSchema } from "../types/zodSchemas";
+import z from "zod";
 
 type Props = {
-	paintingData: WithRequired<Painting, "_id">;
-	seriesId: string;
+  paintingData: WithRequired<Painting, "_id">;
+  seriesId: string;
 };
 
 const ArtCard = ({ paintingData, seriesId }: Props) => {
-	return (
-		<div className="art-card">
-			<Link
-				key={paintingData._id}
-				href={"/gallery/" + seriesId + "/" + paintingData._id}
-			>
-				<NextImage
-					alt={paintingData.name}
-					src={paintingData.image.asset.url}
-					width={paintingData.image.asset.metadata.dimensions.width}
-					height={paintingData.image.asset.metadata.dimensions.height}
-					placeholder="blur"
-					blurDataURL={paintingData.image.asset.metadata.lqip}
-				/>
-				<h2>{paintingData.name}</h2>
-			</Link>
-		</div>
-	);
+  //   const imageProps = useSanityImage(sClient, paintingData.image.asset._id);
+
+  return (
+    <div className="art-card">
+      <Link
+        key={paintingData._id}
+        href={"/gallery/" + seriesId + "/" + paintingData._id}
+      >
+        <NextImage
+          alt={paintingData.name}
+          src={paintingData.image.asset.url}
+          width={paintingData.image.asset.metadata.dimensions.width}
+          height={paintingData.image.asset.metadata.dimensions.height}
+          placeholder="blur"
+          blurDataURL={paintingData.image.asset.metadata.lqip}
+        />
+        <h2>{paintingData.name}</h2>
+      </Link>
+    </div>
+  );
 };
 
 export default ArtCard;
 function useNextSanityImage() {
-	throw new Error("Function not implemented.");
+  throw new Error("Function not implemented.");
 }
--- app/components/GalleryDropDown.tsx
+++ app/components/GalleryDropDown.tsx
@@ -1,74 +1,66 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import PaintingsDropDown from "./PaintingsDropDown";
 
 const GalleryDropDown = () => {
-	const router = useRouter();
-	const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
 
-	useEffect(() => {
-		setIsOpen(false);
-	}, []);
-	useEffect(() => {
-		setIsOpen(false);
-	}, [router.pathname]);
-	return (
-		<div>
-			<button
-				onClick={() => setIsOpen((prev) => !prev)}
-				className="nav-link-btn flex flex-row"
-			>
-				Gallery
-				{isOpen ? (
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 24 24"
-						fill="currentColor"
-						className="w-5 h-5 pt-.75 text-center"
-					>
-						<path
-							fillRule="evenodd"
-							d="M12 20.25a.75.75 0 01-.75-.75V6.31l-5.47 5.47a.75.75 0 01-1.06-1.06l6.75-6.75a.75.75 0 011.06 0l6.75 6.75a.75.75 0 11-1.06 1.06l-5.47-5.47V19.5a.75.75 0 01-.75.75z"
-							clipRule="evenodd"
-						/>
-					</svg>
-				) : (
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 24 24"
-						fill="currentColor"
-						className="w-5 h-5 pt-.75"
-					>
-						<path
-							fillRule="evenodd"
-							d="M12 3.75a.75.75 0 01.75.75v13.19l5.47-5.47a.75.75 0 111.06 1.06l-6.75 6.75a.75.75 0 01-1.06 0l-6.75-6.75a.75.75 0 111.06-1.06l5.47 5.47V4.5a.75.75 0 01.75-.75z"
-							clipRule="evenodd"
-						/>
-					</svg>
-				)}
-			</button>
-			{isOpen && (
-				<div className="bg-transparent absolute top-20 text-gray-800 flex flex-col items start rounded-lg p-2">
-					<Link
-						className="gallery-dropdown-item"
-						href="/gallery/painting"
-					>
-						{" "}
-						Paintings
-					</Link>
-					<PaintingsDropDown isOpen={isOpen} />
-					<Link
-						className="gallery-dropdown-item"
-						href="/gallery/photograph"
-					>
-						{" "}
-						Photography
-					</Link>
-				</div>
-			)}
-		</div>
-	);
+  useEffect(() => {
+    setIsOpen(false);
+  }, []);
+  useEffect(() => {
+    setIsOpen(false);
+  }, [router.pathname]);
+  return (
+    <div>
+      <button
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="nav-link-btn flex flex-row"
+      >
+        Gallery
+        {isOpen ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-5 h-5 pt-.75 text-center"
+          >
+            <path
+              fillRule="evenodd"
+              d="M12 20.25a.75.75 0 01-.75-.75V6.31l-5.47 5.47a.75.75 0 01-1.06-1.06l6.75-6.75a.75.75 0 011.06 0l6.75 6.75a.75.75 0 11-1.06 1.06l-5.47-5.47V19.5a.75.75 0 01-.75.75z"
+              clipRule="evenodd"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-5 h-5 pt-.75"
+          >
+            <path
+              fillRule="evenodd"
+              d="M12 3.75a.75.75 0 01.75.75v13.19l5.47-5.47a.75.75 0 111.06 1.06l-6.75 6.75a.75.75 0 01-1.06 0l-6.75-6.75a.75.75 0 111.06-1.06l5.47 5.47V4.5a.75.75 0 01.75-.75z"
+              clipRule="evenodd"
+            />
+          </svg>
+        )}
+      </button>
+      {isOpen && (
+        <div className="bg-transparent absolute top-20 text-gray-800 flex flex-col items start rounded-lg p-2">
+          <Link className="gallery-dropdown-item" href="/gallery/painting">
+            {" "}
+            Paintings
+          </Link>
+          <Link className="gallery-dropdown-item" href="/gallery/photograph">
+            {" "}
+            Photography
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default GalleryDropDown;
