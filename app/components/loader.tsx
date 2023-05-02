2f5e589515c5f6a8896c384ae5e6f2467d6cb72a
Dean <deanscottg@gmail.com>
2023-05-02 15:47:29 -0400

    loading state

---
+++ app/components/loader.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Image from "next/image";
+import LoaderSVG from "./svgs/Loadersvg";
+
+const loader = () => {
+  return (
+    <div className="flex flex-col h-screen w-full  text-black text-sm ">
+      <div>
+        <LoaderSVG className="h-[10rem] w-[10rem]" />
+      </div>
+      <p className="text-center text-lg">Loading Art for you!</p>
+    </div>
+  );
+};
+
+export default loader;
