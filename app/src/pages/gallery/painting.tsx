aa5ac75f82c6776c58ddb02509639485c1b0f4d3
Kenton Duprey <kentonduprey@icloud.com>
2023-09-24 14:28:43 -0400

    Removing old series pages

--- app/src/pages/gallery/painting.tsx
+++ app/src/pages/gallery/painting.tsx
@@ -7,53 +7,56 @@ import { seriesSchema } from "../../../types/zodSchemas";
 import { z } from "zod";
 
 export const getStaticProps: GetStaticProps = async () => {
-  const seriesDataResponse = await nextSanityClient.fetch(
-    groq`*[_type == 'series']{
+	const seriesDataResponse = await nextSanityClient.fetch(
+		groq`*[_type == 'series']{
             _id,
             name
         }`
-  );
+	);
 
-  const seriesData = z
-    //   Create Array schema to validate
-    .array(
-      // Using the seriesSchema, only selecting the fields to test for
-      seriesSchema.pick({
-        _id: true,
-        name: true,
-      })
-    )
-    // Validating the data
-    .parse(seriesDataResponse);
+	const seriesData = z
+		//   Create Array schema to validate
+		.array(
+			// Using the seriesSchema, only selecting the fields to test for
+			seriesSchema.pick({
+				_id: true,
+				name: true,
+			})
+		)
+		// Validating the data
+		.parse(seriesDataResponse);
 
-  return {
-    props: { seriesData },
-  };
+	return {
+		props: { seriesData },
+	};
 };
 const Paintings = ({
-  seriesData,
+	seriesData,
 }: {
-  seriesData: Pick<Series, "_id" | "name">[];
+	seriesData: Pick<Series, "_id" | "name">[];
 }) => {
-  return (
-    <div className="page-container h-screen">
-      <div className="py-4">
-        <h1 className="text-center  py-2">Paintings and Editioned Prints</h1>
-        <p className="my-10 mx-60 text-center">
-          All original, one-of-a-kind paintings and photographs are available.
-          Limited and numbered editions of the originals are reproduced as
-          museum quality prints on canvas or paper. Canvas prints are stretched
-          over 1 ½ -inch wood mounts and are ready to hang without framing.
-          Prints on acid free deckle edge paper (feathery edges) are unframed or
-          come with the option to be matted and framed to tastefully present the
-          art and photographs.
-        </p>
-        <PaintingNav seriesData={seriesData}></PaintingNav>
-      </div>
+	return (
+		<div className="page-container h-screen">
+			<div className="py-4">
+				<h1 className="text-center  py-2">
+					Paintings and Editioned Prints
+				</h1>
+				<p className="my-10 mx-60 text-center">
+					All original, one-of-a-kind paintings and photographs are
+					available. Limited and numbered editions of the originals
+					are reproduced as museum quality prints on canvas or paper.
+					Canvas prints are stretched over 1 ½ -inch wood mounts and
+					are ready to hang without framing. Prints on acid free
+					deckle edge paper (feathery edges) are unframed or come with
+					the option to be matted and framed to tastefully present the
+					art and photographs.
+				</p>
+				<PaintingNav seriesData={seriesData}></PaintingNav>
+			</div>
 
-      {/* <ArtCard /> */}
-    </div>
-  );
+			{/* <ArtCard /> */}
+		</div>
+	);
 };
 
 export default Paintings;
